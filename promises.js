3570a35a8eedc34da6624d160eb6bc2e488ef83f
Binoth <binothsakith011@gmail.com>
2026-02-16 18:36:11 +0530

    Promises example

---
+++ promises.js
@@ -0,0 +1,10 @@
+﻿const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) resolve('Success!');
+  else reject('Failure!');
+});
+
+myPromise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
